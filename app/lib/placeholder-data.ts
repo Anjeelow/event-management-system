e2e858810e00d24ff33572313a4c9ca4dd4f3b43
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2024-11-30 15:05:57 +0800

    feat: add placeholder for the placeholder

---
+++ app/lib/placeholder-data.ts
@@ -0,0 +1,17 @@
+const users = [
+    {
+      id: '',
+      first_name: 'Reece',
+      last_name: 'Lim',
+      email: 'reece@gmail.com',
+      // add the rest here
+    },
+];
+
+const events = [
+    {
+        // add events
+    }
+]
+
+// add the rest of the table data
