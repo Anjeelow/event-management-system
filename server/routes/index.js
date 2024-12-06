af5c73a4c1e46f5dbf59d9e826c3bc4219b747a4
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2024-12-06 16:01:45 +0800

    feat: add POST /signup API with validation

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import userRouter from "./userRoutes.js";
 import eventRouter from "./eventRoutes.js";
+import signup from "./signup.js"
 
 const router = Router();
 
 router.use(userRouter);
 router.use(eventRouter);
+router.use(signup);
 console.log("Ive been reached!");
 
 export default router;
