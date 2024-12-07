8f7509ed8d0b979c596ed63cd2f19e1d9206f804
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2024-12-08 02:33:00 +0800

    feat: add token validation and auth for /profile

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { query } from "../db-service.js";
+import authenticateToken from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
@@ -11,5 +12,28 @@ router.get("/api/users", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+
+router.get("/api/users/:id", authenticateToken, async (req, res) => {
+  try {
+    const user_id = req.params.id;
+
+    if (!user_id) {
+      return res.status(400).json({ message: 'User ID is required' })
+    }
+
+    const sql = 'SELECT * FROM User WHERE user_id = ?'
+    const result = await query(sql, [user_id])
+
+    if (result.length === 0) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+
+    return res.status(200).json({user: result[0]})
+
+  } catch (error) {
+    console.error('Error fetching user:', error)
+    return res.status(500).json({ message: 'Internal server error' })
+  }
+})
 
 export default router;
