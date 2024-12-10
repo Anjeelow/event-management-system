58c6f70dc13cdcba2f8c2b7464ca8a1e7752c186
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2024-12-10 23:13:14 +0800

    feat: add calendar to edit start and end time for events

---
+++ client/components/date-picker.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import TimePicker from "react-datepicker";
+import { subDays } from "react-datepicker/dist/date_utils";
+import 'react-datepicker/dist/react-datepicker.css'
+import { CiCalendar } from "react-icons/ci";
+
+export default function CalendarInput({ 
+    date, 
+    setDate 
+} : { 
+    date: Date | null, 
+    setDate: (date: Date | null) => void
+}) {
+
+    const [defaultDate, setDefaultDate] = useState(new Date(date))
+
+    useEffect(() => {
+        const newDate = new Date(date)
+        setDefaultDate(newDate)
+    }, [date])
+
+    const handleClick = (newDate: any) => {
+        setDate(newDate)
+        setDefaultDate(newDate)
+    }
+
+    return (
+        <div className="flex items-center">
+            <label className="flex items-center">
+                <TimePicker  
+                    showTimeSelect 
+                    dateFormat='Pp'
+                    className="p-2 w-full border-2 border-gray-100" 
+                    minDate={new Date()}
+                    onChange={newDate => {handleClick(newDate)}}
+                    selected={defaultDate}
+                />
+                <CiCalendar className="opacity-40 ml-2 cursor-pointer" size={30} />
+            </label>
+        </div>
+    )
+}
